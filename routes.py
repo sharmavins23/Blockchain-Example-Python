b191dc5741a22f1726002ede926b480a69a3f022
Vins <sharmavins23@gmail.com>
2020-06-28 01:37:43 -0400

    👽 exposed server shutdown route

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from main import app
+import os
 
 # Route to 'mine' (post) a new block
 
@@ -8,4 +9,16 @@ from main import app
 
 # Route to resolve neighboring chains (get the longest one from friends)
 
+
 # Route to shut down our server (don't put these on actual applications, please)
+@app.route("/terminate", methods=["GET"])
+def shutdown_server():
+    try:
+        # Emulate a CTRL+C event to stop code function
+        CTRL_C_EVENT = 0  # Value storage
+        GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0)
+        return "Worked"
+    except Exception as e:
+        # In case of failure, make the operating system shut things down
+        os._exit(0)  # This will probably throw a nasty error, but it WILL work
+        return e
