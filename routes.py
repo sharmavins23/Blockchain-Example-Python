079506c54494cd3a0d59780cdb9e5a401f4f8237
Vins <sharmavins23@gmail.com>
2020-07-14 17:38:14 -0400

    ✨ added blockchain mining route

--- routes.py
+++ routes.py
@@ -20,7 +20,32 @@ def teapot():
 
     return jsonify(response), 418
 
+
 # Route to 'mine' (post) a new block
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    blockchain.new_transaction(
+        sender="0",  # Server
+        recipient=node_identifier,
+        amount=1
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block Forged!',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
+
 
 # Route to post a new transaction
 
